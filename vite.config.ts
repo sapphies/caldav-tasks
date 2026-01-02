dda2c98ae3447110d52e881b6f4860255cce66e9
Chloe A <chloe@sapphic.moe>
2026-01-02 11:26:13 +0500

    chore(vite): remove visualizer now that it's no longer necessary

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   plugins: [react()],
